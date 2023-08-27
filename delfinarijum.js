36fbba75acb3c9da3bd24252bc7973b05d7be8a5
Dinonononono1989 <120413730+Dinonononono1989@users.noreply.github.com>
2023-08-27 21:29:14 +0200

    first commit

---
+++ delfinarijum.js
@@ -0,0 +1,31 @@
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
+
+var grammer = '#JSGF V1.0';
+
+var recognition = new SpeechRecognition();
+var speechRecognitionGrammerList = new SpeechGrammarList();
+speechRecognitionGrammerList.addFromString(grammer, 1);
+
+recognition.grammers = speechRecognitionGrammerList;
+recognition.lang = 'sr-SP';
+recognition.interimResult = false;
+document.body.style.backgroundColor = "blue";
+
+recognition.onresult = function (event) {
+    var last = event.results.length - 1;
+    var command = event.results[last][0].transcript;
+    message.textContent = 'Речено је: ' + command + '.';
+    window.open(`https://delfi.rs/pretraga?c=0&q=${command}`);
+}
+recognition.onspeechend = function () {
+    //document.body.style.backgroundColor = "white";
+    //message.textContent = 'Чича мича, мора да се прича!';
+};
+recognition.onerror = function (event) {
+    console.log(event.error);
+    message.textContent = 'Чича мича, мора да се прича!';
+}
+document.querySelector('#btnGiveCommand').addEventListener('click', function () {
+    recognition.start();
+});
