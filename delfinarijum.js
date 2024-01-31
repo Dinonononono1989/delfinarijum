249b060c05337cecc590605f0f7dd2661fd34184
Dinonononono1989 <andriano.djordjevic@yahoo.com>
2024-01-31 22:05:27 +0100

    proba

--- delfinarijum.js
+++ delfinarijum.js
@@ -1,35 +1,41 @@
-var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
-var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
-
-var grammer = '#JSGF V1.0';
-
-var recognition = new SpeechRecognition();
-var speechRecognitionGrammerList = new SpeechGrammarList();
-speechRecognitionGrammerList.addFromString(grammer, 1);
-
-recognition.grammers = speechRecognitionGrammerList;
-recognition.lang = 'sr-SP';
-recognition.interimResult = false;
-document.body.style.backgroundColor = "blue";
-
-recognition.onresult = function (event) {
-    var last = event.results.length - 1;
-    var command = event.results[last][0].transcript;
-    message.textContent = 'Речено је: ' + command + '.';
-    window.open(`https://delfi.rs/pretraga?c=0&q=${command}`);
-}
-//recognition.onspeechend = function () {
-    //document.body.style.backgroundColor = "white";
-    //message.textContent = 'Чича мича, мора да се прича!';
-//};
-recognition.onerror = function (event) {
-    console.log(event.error);
-    message.textContent = 'Чича мича, мора да се прича!';
-}
-let touchEvent = 'ontouchstart' in window ? 'touchstart' : 'click';
-document.querySelector('#btnGiveCommand').addEventListener(touchEvent, recognition.start);
-
-document.querySelector('#btnGiveCommand').addEventListener('click', function () {
+ document.querySelector('#btnGiveCommand').addEventListener('click', function () {
     alert('proba');
-    recognition.start();
-});
+    //recognition.start();
+ });
+
+// var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+// var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
+
+// var grammer = '#JSGF V1.0';
+
+// var recognition = new SpeechRecognition();
+// var speechRecognitionGrammerList = new SpeechGrammarList();
+// speechRecognitionGrammerList.addFromString(grammer, 1);
+
+
+// recognition.grammers = speechRecognitionGrammerList;
+// recognition.lang = 'sr-SP';
+// recognition.interimResult = false;
+// document.body.style.backgroundColor = "blue";
+
+// recognition.onresult = function (event) {
+//     var last = event.results.length - 1;
+//     var command = event.results[last][0].transcript;
+//     message.textContent = 'Речено је: ' + command + '.';
+//     window.open(`https://delfi.rs/pretraga?c=0&q=${command}`);
+// }
+// //recognition.onspeechend = function () {
+//     //document.body.style.backgroundColor = "white";
+//     //message.textContent = 'Чича мича, мора да се прича!';
+// //};
+// recognition.onerror = function (event) {
+//     console.log(event.error);
+//     message.textContent = 'Чича мича, мора да се прича!';
+// }
+// let touchEvent = 'ontouchstart' in window ? 'touchstart' : 'click';
+// document.querySelector('#btnGiveCommand').addEventListener(touchEvent, recognition.start);
+
+// document.querySelector('#btnGiveCommand').addEventListener('click', function () {
+//     alert('proba');
+//     recognition.start();
+// });
