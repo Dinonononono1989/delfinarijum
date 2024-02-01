e32ca7f1afb1916fad896cdd66d96f00f7c57a31
Dinonononono1989 <120413730+Dinonononono1989@users.noreply.github.com>
2024-02-01 13:11:52 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import Dugme from './Dugme';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <p>Добродошли на</p>
+      <p>ДЕЛФИНАРИЈУМ</p>
+      <p>-књижаревог најбољег другара!</p>
+      <Dugme />
+      <p>Кликни на дугме и изговори назив књиге.</p>
+      <p>У новом прозору ће се отворити претрага на сајту www.delfi.rs</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
